d2c91f47ce783ed7814261b15cd44f8b5807b543
Myrian Cristina Silva <73667019+myriancristina@users.noreply.github.com>
2021-03-06 01:21:04 -0300

    Add files via upload

---
+++ myrianc/Enum/Genero.cs
@@ -0,0 +1,22 @@
+namespace DIO.Series
+{
+	public enum Genero
+	{
+		Ação = 1,
+		Aventura = 2,
+		Biográfico = 3,
+		Comédia = 4,
+		Histórico = 5,
+		Fantasia = 6,
+		Drama = 7,
+		Musical = 8,
+		Romance = 9,
+		Suspense = 10,
+		Terror = 11,
+		Dança = 12,
+		Faroeste = 13,
+		Policial = 14,
+		Desenho = 15
+
+	}
+}
